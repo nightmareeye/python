03af2c4746716583ca693ca96950c6cd94692d68
nightmareeye <mrrexcrafter@gmail.com>
2022-04-21 01:09:19 +0300

    Added test for create

--- pz3/tests/test_main.py
+++ pz3/tests/test_main.py
@@ -1,3 +1,6 @@
+from base64 import encode
+from encodings import utf_8
+import json
 from src.TodoJournal import TodoJournal
 
 def test_init():
@@ -10,3 +13,32 @@ def test_init():
     expected_name = "test3"
     assert entries == expected_entries
     assert name == expected_name
+
+def test_create_journal(tmpdir):
+    todo_filename = "test_todo"
+    todo = tmpdir.join(todo_filename)
+    TodoJournal.create(todo, "test")
+
+    expected_todo = json.dumps(
+        {
+        "name": "test",
+        "todos": []
+        },
+        indent=4)
+    assert expected_todo == todo.read()
+
+def test_add_entry(tmpdir):
+    todo_filename = "test_todo"
+    todo = tmpdir.join(todo_filename)
+    TodoJournal.create(todo, "test")
+    todo_jrnl = TodoJournal(todo)
+    todo_jrnl.add_entry("Сходить за молоком")
+
+    expected_todo = json.dumps(
+        {
+        "name": "test",
+        "todos": ["Сходить за молоком"]
+        },
+        indent=4,
+        ensure_ascii=False,)
+    assert expected_todo == todo.read()
